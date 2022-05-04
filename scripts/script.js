95b604f0a9d42495b69544a4080fd1a267686aaf
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-05-04 21:16:42 +0200

    fetch

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const url = "https://www.espenpedersen.no/exam1/wp-json/wp/v2/"
+
+fetch(url)
+ .then (response => response.json())
+ .then (data => 
+    {
+        listData(data)
+        console.log(data);
+    })
+    
+ .catch((error) => {
+     console.error('Error:' + error);
+});
+
+function listData (data) {
+    
+}
