b7bf3fb78d772190ea328d5fbdb3f2245494afe6
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-05-07 21:49:04 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const url = "https://espenpedersen.no/exam1/wp-json/wp/v2/posts?_embed";
-const output = document.querySelector(".API__output");
+const output = document.querySelector(".API-output");
 
 fetch(url)
  .then(response => response.json())
@@ -15,14 +15,15 @@ fetch(url)
     for (let post of posts) {
         output.innerHTML += `
         <a href="./blogpost.html?id=${post.id}">
-            <div class="carousel__card">
+            <div class="API-output__card">
                 <img src="${post._embedded['wp:featuredmedia']['0'].source_url}" alt="${post._embedded['wp:featuredmedia']['0'].alt_text}" class="image-style">
                 <p>${post.date}</p>
                 <h2>${post.title.rendered}</h2>
             </div>
-        </a>`
+        </a>` 
     }
  }
+
 
 //  Hamburger menu
 
@@ -44,13 +45,13 @@ menuBtnClose.addEventListener("click", () => {
 
 // Scroll effect
 
-window.onscroll = function () {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction () {
     const headerShadow = document.querySelector("#header-shadow")
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         headerShadow.classList.add("header-shadow__scroll");
     } else {
-        headerShadow.classList.remove("header-shadow__scroll")
+        headerShadow.classList.remove("header-shadow__scroll");
     }
 }
